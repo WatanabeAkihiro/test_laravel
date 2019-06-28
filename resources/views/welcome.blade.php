272f486042bf62d7d4c86fcd9e2d64a1ea879956
Akihiro Watanabe <watakihiro@gmail.com>
2019-06-28 18:56:46 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-body">
+                    This is not Your Application's Landing Page.<br>
+                    Amazon web service
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
